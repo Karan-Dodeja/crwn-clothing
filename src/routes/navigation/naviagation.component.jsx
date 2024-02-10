6b67a5521866ad472a68d75295680f9652192fac
kdodeja62@gmail.com <kdodeja62@gmail.com>
2024-02-11 00:28:35 +0530

    Navigation Style and Added Logo

--- src/routes/navigation/naviagation.component.jsx
+++ src/routes/navigation/naviagation.component.jsx
@@ -1,12 +1,14 @@
 import { Outlet, Link } from "react-router-dom";
 import { Fragment } from "react";
+import { Logo } from "../../assets/crown.svg";
+import "./navigation.styles.scss";
 
 export const Navigation = () => {
   return (
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to="/">
-          <div>Logo</div>
+          <div>{Logo}</div>
         </Link>
         <div className="links-container">
           <Link className="nav-link" to="/shop">
