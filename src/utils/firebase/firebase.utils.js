cbf7d0775da0299edf091db11d0f5c3e60b85869
kdodeja62@gmail.com <kdodeja62@gmail.com>
2024-02-11 21:15:06 +0530

    Create user document in firestore

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,7 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
 } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDLggckzKITWdctrsqa1v_YDDo80T8DJ6w",
@@ -26,5 +27,26 @@ export const auth = getAuth();
 
 export const signInWithGooglePopup = () => {
   signInWithPopup(auth, provider);
+};
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log(error.messagef);
+    }
+  }
+  return userDocRef;
 };
 
