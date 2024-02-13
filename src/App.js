502a9ddb00a0c5e308616a36284d2e20212a93f7
kdodeja62@gmail.com <kdodeja62@gmail.com>
2024-02-13 23:19:28 +0530

    Created Shop Component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Navigation from "./routes/navigation/naviagation.component";
 import Home from "./routes/home/home.component";
 import "./categories.styles.scss";
 import { Authentication } from "./routes/authentication/authentication.component";
+import { Shop } from "./routes/shop/shop.component";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="shop" />
+        <Route path="shop" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
