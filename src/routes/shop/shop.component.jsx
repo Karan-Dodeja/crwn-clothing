0bd489a327eddc29ac56b054a082f60a0851a8a3
kdodeja62@gmail.com <kdodeja62@gmail.com>
2024-02-21 01:24:19 +0530

    Created Category Page

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,12 +1,13 @@
-import { Routes, Route } from "react-router-dom"; 
+import { Routes, Route } from "react-router-dom";
 import "./shop.styles.scss";
 import { CategoriesPreview } from "../categories-preview/categories-preview.routes";
+import { Category } from "../category/category.component";
 
 export const Shop = () => {
-  
   return (
     <Routes>
-      <Route  index element={<CategoriesPreview />} />
+      <Route index element={<CategoriesPreview />} />
+      <Route path="/category" element={<Category />} />
     </Routes>
   );
 };
