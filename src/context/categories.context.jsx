a4d36a3fdd8e30b515c426ed8d3c5d83d3c0478b
kdodeja62@gmail.com <kdodeja62@gmail.com>
2024-02-21 00:55:03 +0530

    useCategoriesMap

--- src/context/categories.context.jsx
+++ src/context/categories.context.jsx
@@ -2,24 +2,25 @@ import { createContext, useEffect, useState } from "react";
 
 import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
 
-import SHOP_DATA from "../shop-data.js";
-
-export const ProductContext = createContext({
-  products: [],
+export const CategoriesContext = createContext({
+  categoriesMap: {},
 });
 
-export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState([]);
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
   useEffect(() => {
     const getCategoriesMaps = async () => {
       const categoryMap = await getCategoriesAndDocuments();
       console.log(categoryMap);
+      setCategoriesMap(categoryMap);
     };
 
     getCategoriesMaps();
   });
-  const value = { products };
+  const value = { categoriesMap };
   return (
-    <ProductContext.Provider value={value}>{children}</ProductContext.Provider>
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
   );
 };
