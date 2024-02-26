38280b916f8af68a523f9d0e15f9cd7296a251af
kdodeja62@gmail.com <kdodeja62@gmail.com>
2024-02-26 14:23:20 +0530

    Using use selector redux hook

--- src/routes/navigation/naviagation.component.jsx
+++ src/routes/navigation/naviagation.component.jsx
@@ -1,4 +1,4 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { Fragment, useContext } from "react";
 import { Logo } from "../../assets/crown.svg";
 import {
@@ -7,14 +7,17 @@ import {
   NavLinks,
   NavLink,
 } from "./navigation.styles";
+import { useSelector } from "react-redux";
 import { CartDropdown } from "../../components/cart-dropdown/cart-dropdown.compnent";
 import { CartIcon } from "../../components/cart-icon/cart-icon.component";
 import { UserContext } from "../../context/user.context";
 import { CartContext } from "../../context/cart.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
+import { selectCurrentUser } from "../../store/user/user.selector";
 
 export const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
+  const currentUser = useSelector(selectCurrentUser);
+  //  const { currentUser } = useContext(UserContext);
   const { isCartOpen } = useContext(CartContext);
   return (
     <Fragment>
